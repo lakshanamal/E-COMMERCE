9b2fe938e56dbda013ceb2608c5aa5b35228aa73
lakshanamal <lakshanamal100@gmail.com>
2021-05-21 23:53:17 +0530

    database connected

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-const express = require("exxpress");
+const express = require("express");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+app.use(express.json());
+
+mongoose.connect(
+  "mongodb+srv://lakshan:lakshan@cluster0.ja8ud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  }
+);
+app.listen(3001, () => {
+  console.log("Server listing port 3001");
+});
