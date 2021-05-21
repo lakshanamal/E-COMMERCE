91746d15cdf54d1dc9b81e5a2cd9ace0eca6dcd5
lakshanamal <lakshanamal100@gmail.com>
2021-05-21 23:56:47 +0530

    env file added

--- server/index.js
+++ server/index.js
@@ -6,13 +6,10 @@ const mongoose = require("mongoose");
 dotenv.config();
 app.use(express.json());
 
-mongoose.connect(
-  "mongodb+srv://lakshan:lakshan@cluster0.ja8ud.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-  }
-);
+mongoose.connect(process.env.MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
 app.listen(3001, () => {
   console.log("Server listing port 3001");
 });
