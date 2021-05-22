32370aa94ff01f29734d9d172143f953c5f45dc5
lakshanamal <lakshanamal100@gmail.com>
2021-05-22 10:05:30 +0530

    register feild validate

--- server/controllers/userCtr.js
+++ server/controllers/userCtr.js
@@ -1,18 +1,20 @@
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
+const { registerAuth } = require("./validation");
 const userCtr = {
   register: async (req, res) => {
+    const {error} = registerAuth(req.body);
+    if (error) return res.status(400).json(error.details[0].message);
     try {
       const { name, email, password } = req.body; // object distraction
       const salt = await bcrypt.genSalt(10);
       const hashPassword = await bcrypt.hash(password, salt);
       const newUser = new User({
-        name: req.body.name,
-        email: req.body.email,
+        name: name,
+        email: email,
         password: hashPassword,
       });
-      const savedUser=await newUser.save();
-      return res.json(savedUser);
+      const savedUser = await newUser.save();
     } catch (err) {
       return res.status(500).json({ msg: err.message });
     }
