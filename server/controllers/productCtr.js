fd63d8fb5d36bb3fe9f9d3871a5361a9851285f5
lakshanamal <lakshanamal100@gmail.com>
2021-05-23 23:38:46 +0530

    product update added

--- server/controllers/productCtr.js
+++ server/controllers/productCtr.js
@@ -21,21 +21,29 @@ const productCtrl={
             })
             await newProduct.save()
 
-            res.json(newProduct);
+            res.json({msg:"new Product added sucessfull"});
         }catch(err){
             return res.status(500).json({msg:err.message})
         }
     },
     deleteProduct:async (req,res)=>{
         try{
-
+            await Product.findByIdAndDelete(req.paras.id)
+            res.json({msg:"Product deleted"});
         }catch(err){
             return res.status(500).json({msg:err.message})
         }
     },
     updateProduct:async (req,res)=>{
         try{
+            const {title,price,discription,content,images,category}=req.body;
+            if(!images) return res.status(400).json({msg:"Image not uploaded"});
 
+            await Product.findByIdAndUpdate({_id:req.params.id},{
+                title:title.toLowerCase(),price,discription,content,images,category
+            })
+
+            res.json({msg:"Updated a product"})
         }catch(err){
             return res.status(500).json({msg:err.message})
         }
