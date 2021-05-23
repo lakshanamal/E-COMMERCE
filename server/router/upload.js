2b883fa053b5192caa42f21eda99fb9100dd4253
lakshanamal <lakshanamal100@gmail.com>
2021-05-23 20:51:03 +0530

    image delete function added

--- server/router/upload.js
+++ server/router/upload.js
@@ -4,7 +4,7 @@ const express = require("express");
 const auth = require("../middleware/auth");
 const authAdmin = require("../middleware/authAdmin");
 const env = require("dotenv");
-const fs =require('fs');
+const fs = require("fs");
 
 cloudinary.config({
   cloud_name: "dtsfx4bxo",
@@ -13,30 +13,45 @@ cloudinary.config({
 });
 
 router.post("/upload", (req, res) => {
-  //   try {
-  if (!req.files || Object.keys(req.files).length == 0)
-    return res.status(400).json({ msg: "No files were uploaded" });
-  const file = req.files.file;
-  if (file.size > 1024 * 1024) {
-    removeTmp(file.tempFilePath);
-    return res.status(400).json({ msg: "Size too large" });
-  }
-
-  if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png"){
-    removeTmp(file.tempFilePath);
-    return res.status(400).json({ msg: "File format is incorrect" });
-  }
-    
-  cloudinary.v2.uploader.upload(
-    file.tempFilePath,
-    { folder: "test" },
-    (err, result) => {
-      if (err) throw err;
-
-      res.json({ public_id: result.public_id, url: result.secure_url });
+  try {
+    if (!req.files || Object.keys(req.files).length == 0)
+      return res.status(400).json({ msg: "No files were uploaded" });
+    const file = req.files.file;
+    if (file.size > 1024 * 1024) {
+      removeTmp(file.tempFilePath);
+      return res.status(400).json({ msg: "Size too large" });
     }
-  );
-  removeTmp(file.tempFilePath);
+
+    if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png") {
+      removeTmp(file.tempFilePath);
+      return res.status(400).json({ msg: "File format is incorrect" });
+    }
+
+    cloudinary.v2.uploader.upload(
+      file.tempFilePath,
+      { folder: "test" },
+      (err, result) => {
+        if (err) throw err;
+        removeTmp(file.tempFilePath);
+        res.json({ public_id: result.public_id, url: result.secure_url });
+      }
+    );
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
+});
+
+router.post("/destory", (req, res) => {
+  try {
+    const { public_id } = req.body;
+    if (!public_id) return res.status(400).json({ msg: "No image select" });
+    cloudinary.v2.uploader.destroy(public_id, (err, result) => {
+      if (err) throw err;
+      res.json({ msg: "Image Deleted" });
+    });
+  } catch (err) {
+    return res.status(500).json({ msg: err.message });
+  }
 });
 
 const removeTmp = (path) => {
