dd3565d56307b07de79f9e417eb79e5ddfe56fd0
lakshanamal <lakshanamal100@gmail.com>
2021-05-23 19:20:57 +0530

    file upload configuration done

--- server/router/upload.js
+++ server/router/upload.js
@@ -1,21 +1,48 @@
 const router = require("express").Router();
 const cloudinary = require("cloudinary");
+const express = require("express");
 const auth = require("../middleware/auth");
 const authAdmin = require("../middleware/authAdmin");
+const env = require("dotenv");
+const fs =require('fs');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  COUDINARY_API_KEY:  process.env.COUDINARY_API_KEY,
-  api_key:  process.env.CLOUDNARY_SECRET,
+  cloud_name: "dtsfx4bxo",
+  api_key: "356946675641672",
+  api_secret: "aReRitaRfNQxtwbHI7mgqAmwS54",
 });
 
-router.post('/upload',(req,res)=>{
-    try{
-        console.log( req.files);
-        res.json('text upload')
-    }catch(err){
-        return res.status(500).json({msg:err.message})
-    }
-})
+router.post("/upload", (req, res) => {
+  //   try {
+  if (!req.files || Object.keys(req.files).length == 0)
+    return res.status(400).json({ msg: "No files were uploaded" });
+  const file = req.files.file;
+  if (file.size > 1024 * 1024) {
+    removeTmp(file.tempFilePath);
+    return res.status(400).json({ msg: "Size too large" });
+  }
 
-module.exports=router ;
+  if (file.mimetype !== "image/jpeg" && file.mimetype !== "image/png"){
+    removeTmp(file.tempFilePath);
+    return res.status(400).json({ msg: "File format is incorrect" });
+  }
+    
+  cloudinary.v2.uploader.upload(
+    file.tempFilePath,
+    { folder: "test" },
+    (err, result) => {
+      if (err) throw err;
+
+      res.json({ public_id: result.public_id, url: result.secure_url });
+    }
+  );
+  removeTmp(file.tempFilePath);
+});
+
+const removeTmp = (path) => {
+  fs.unlink(path, (err) => {
+    if (err) throw err;
+  });
+};
+
+module.exports = router;
