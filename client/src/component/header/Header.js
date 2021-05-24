c25941319054b9a02fb372e55fdb969d1b9ecad4
lakshanamal <lakshanamal100@gmail.com>
2021-05-25 00:05:57 +0530

    folder structed and bug fix

---
+++ client/src/component/header/Header.js
@@ -0,0 +1,12 @@
+import React,{useState,useContext } from 'react'
+import {GlobalState} from '../../GlobalState'
+
+const Header=()=>{
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Header;
---
+++ client/src/mainpages/pages.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function pages() {
+    return (
+        <div>
+            <h1>Main pages</h1>
+        </div>
+    )
+}
+
+export default pages
