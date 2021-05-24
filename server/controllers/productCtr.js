aaa7138bcf20d761b8e19e754ef5564e76a5775c
lakshanamal <lakshanamal100@gmail.com>
2021-05-24 13:55:42 +0530

    fiter feature done

--- server/controllers/productCtr.js
+++ server/controllers/productCtr.js
@@ -1,53 +1,101 @@
-const Product=require('../models/productModel')
+const Product = require("../models/productModel");
 
-const productCtrl={
-    getProduct:async (req,res)=>{
-        try{
-            const products=await Product.find();
-            res.json(products)
-        }catch(err){
-            return res.status(500).json({msg:err.message})
-        }
-    },
-    createProduct:async (req,res)=>{
-        try{
-            const {product_id,title,price,discription,content,images,category}=req.body;
-            if(!images) return res.status(400).json({msg:"No image uplaoded"})
-            const checkProduct=await Product.findOne({product_id});
-            if(checkProduct) return res.status(400).json({msg:"No image uplaoded"})
+class APIfeature {
+  constructor(query, queryString) {
+    this.query = query;
+    this.queryString = queryString;
+  }
 
-            const newProduct=new Product({
-                product_id,title:title.toLowerCase(),price,discription,content,images,category
-            })
-            await newProduct.save()
+  filtering() {
+    const queryObj = { ...this.queryString };
+    const excludedFeild = ["page", "sort", "limit"];
+    excludedFeild.forEach((el) => delete queryObj[el]);
 
-            res.json({msg:"new Product added sucessfull"});
-        }catch(err){
-            return res.status(500).json({msg:err.message})
-        }
-    },
-    deleteProduct:async (req,res)=>{
-        try{
-            await Product.findByIdAndDelete(req.paras.id)
-            res.json({msg:"Product deleted"});
-        }catch(err){
-            return res.status(500).json({msg:err.message})
-        }
-    },
-    updateProduct:async (req,res)=>{
-        try{
-            const {title,price,discription,content,images,category}=req.body;
-            if(!images) return res.status(400).json({msg:"Image not uploaded"});
+    let queryStr = JSON.stringify(queryObj);
 
-            await Product.findByIdAndUpdate({_id:req.params.id},{
-                title:title.toLowerCase(),price,discription,content,images,category
-            })
-
-            res.json({msg:"Updated a product"})
-        }catch(err){
-            return res.status(500).json({msg:err.message})
-        }
-    },
+    queryStr = queryStr.replace(
+      /\b(gte|gt|lt|lte|regex)\b/g,
+      (match) => "$" + match // conatination with $
+    );
+    this.query.find(JSON.parse(queryStr)); // search database
+    return this;
+  }
+  sorting() {}
+  paginating() {}
 }
+const productCtrl = {
+  getProduct: async (req, res) => {
+    try {
+      const features = new APIfeature(Product.find(), req.query).filtering();
+      const products = await features.query; //member of APIfeature class
+      //   const products=await Product.find();
+      res.json(products);
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  createProduct: async (req, res) => {
+    try {
+      const {
+        product_id,
+        title,
+        price,
+        discription,
+        content,
+        images,
+        category,
+      } = req.body;
+      if (!images) return res.status(400).json({ msg: "No image uplaoded" });
+      const checkProduct = await Product.findOne({ product_id });
+      if (checkProduct)
+        return res.status(400).json({ msg: "No image uplaoded" });
 
-module.exports=productCtrl
+      const newProduct = new Product({
+        product_id,
+        title: title.toLowerCase(),
+        price,
+        discription,
+        content,
+        images,
+        category,
+      });
+      await newProduct.save();
+
+      res.json({ msg: "new Product added sucessfull" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  deleteProduct: async (req, res) => {
+    try {
+      await Product.findByIdAndDelete(req.paras.id);
+      res.json({ msg: "Product deleted" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+  updateProduct: async (req, res) => {
+    try {
+      const { title, price, discription, content, images, category } = req.body;
+      if (!images) return res.status(400).json({ msg: "Image not uploaded" });
+
+      await Product.findByIdAndUpdate(
+        { _id: req.params.id },
+        {
+          title: title.toLowerCase(),
+          price,
+          discription,
+          content,
+          images,
+          category,
+        }
+      );
+
+      res.json({ msg: "Updated a product" });
+    } catch (err) {
+      return res.status(500).json({ msg: err.message });
+    }
+  },
+};
+
+module.exports = productCtrl;
