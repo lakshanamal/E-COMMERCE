7cd0cc874440efbe9b2d414a79611975a20dfcb8
lakshanamal <lakshanamal100@gmail.com>
2021-05-27 19:29:54 +0530

    login basic done

--- client/src/component/auth/Login.js
+++ client/src/component/auth/Login.js
@@ -1,11 +1,60 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 function Login() {
-    return (
-        <div>
-            Login
-        </div>
-    )
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const loginSubmit =async (e) => {
+    e.preventDefault();
+    try {
+        await axios.post('/user/login',{...user,});
+        localStorage.setItem('firstLogin',true)
+        window.location.href='/';
+        
+    } catch (err) {
+        alert(err.response.data)
+    }
+  };
+//   useEffect(() => {
+//       console.log(user.email)
+//       console.log(user.password)
+//   })
+
+  return (
+    <div>
+      <form onSubmit={loginSubmit}>
+        <input
+          type="email"
+          name="email"
+          value={user.email}
+          onChange={onChangeInput}
+          id="email"
+          required
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={onChangeInput}
+          id="password"
+          required
+          placeholder="Password"
+          autoComplete="on"
+        />
+        <button type="submit">Login</button>
+        <Link to="/register">Register</Link>
+      </form>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
