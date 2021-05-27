ba2074b12f7cbdddeecfbd2d21bddaad6d092dd8
lakshanamal <lakshanamal100@gmail.com>
2021-05-27 19:38:06 +0530

    register basic implement

--- client/src/component/auth/Register.js
+++ client/src/component/auth/Register.js
@@ -1,11 +1,69 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 function Register() {
-    return (
-        <div>
-            <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod veritatis quia provident atque modi iste tempore tenetur neque magnam veniam doloremque animi, distinctio, maiores, suscipit vero inventore illum incidunt delectus.</h1>
-        </div>
-    )
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
+
+  const onChangeInput = (e) => {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  };
+
+  const registerSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("/user/register", { ...user });
+      localStorage.setItem("firstLogin", true);
+      window.location.href = "/";
+    } catch (err) {
+      alert(err.response.data);
+    }
+  };
+  //   useEffect(() => {
+  //       console.log(user.email)
+  //       console.log(user.password)
+  //   })
+
+  return (
+    <div>
+      <form onSubmit={registerSubmit}>
+      <input
+          type="text"
+          name="name"
+          value={user.name}
+          onChange={onChangeInput}
+          id="name"
+          required
+          placeholder="Name"
+        />
+        <input
+          type="email"
+          name="email"
+          value={user.email}
+          onChange={onChangeInput}
+          id="email"
+          required
+          placeholder="Email"
+        />
+        <input
+          type="password"
+          name="password"
+          value={user.password}
+          onChange={onChangeInput}
+          id="password"
+          required
+          placeholder="Password"
+          autoComplete="on"
+        />
+        <button type="submit">Register</button>
+        <Link to="/login">Login</Link>
+      </form>
+    </div>
+  );
 }
 
-export default Register
+export default Register;
