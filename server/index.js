26e58e1ce4cbac960d6add2e5074fd1d1ae9708c
lakshanamal <lakshanamal100@gmail.com>
2021-05-28 20:19:37 +0530

    new package added

--- server/index.js
+++ server/index.js
@@ -3,19 +3,24 @@ const app = express();
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
-const fileUpload=require('express-fileupload');
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
 
 dotenv.config();
 app.use(cookieParser());
 app.use(express.json());
-app.use(fileUpload({
-  useTempFiles:true
-}))
+app.use(cors());
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+  })
+);
 // route
 app.use("/user", require("./router/userRouter"));
 app.use("/api", require("./router/categoryRouter"));
-app.use('/api',require('./router/upload'))
-app.use('/api',require('./router/productRoute'))
+app.use("/api", require("./router/upload"));
+app.use("/api", require("./router/productRoute"));
 
 mongoose.connect(process.env.MONGODB_URI, {
   useUnifiedTopology: true,
